0bb5c790a93268056589ecda46ff0cf13dff1e7b
Vishnu <vu3tpz@gmail>
2023-03-06 21:31:19 +0530

    Top Side dropdown update

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import logo from './assets/logo.svg';
 import kids from './assets/kids.svg';
+import { useOnHoverOutside } from "../Hooks/useOnHoverOutside";
 import './NavBar.css';
 
 function NavBar() {
+    const dropdownRef = useRef(null); // Create a reference for dropdown container
+    const [isSideMenu, setSideMenu] = useState(false)
+
+    // Function to close dropdown
+    const closeHoverMenu = () => {
+        setSideMenu(false);
+    };
+
+    useOnHoverOutside(dropdownRef, closeHoverMenu); // Call the hook
     return (
-        <div className='navbar'>
-            <a href='#' className='bi-a'><i class="bi bi-list"></i></a>
+        <div className='navbar' ref={dropdownRef}>
+            <i class="bi bi-list" onMouseOver={() => setSideMenu(true)}></i>
+            {isSideMenu && <SideMenu />}
             <img src={logo} alt="Logo" className="brand-logo"/>
             <ul className="nav-links">
                 <li className="nav-items"><a href="#">TV</a></li>
@@ -16,10 +27,25 @@ function NavBar() {
                 <li className="nav-items"><a href='#'><img src={kids} alt="Kids" /></a></li>
             </ul>
             <div className="right-container">
-                <input type="text" className="search-box" placeholder="search" />
+                <div className="inputWithIcon">
+                    <input type="text" className="search-box" placeholder="search" />
+                    <ion-icon name="search-outline"></ion-icon>
+                </div>
                 <button className="sub-btn">subscribe</button> 
                 <a href="#" className="login-link">Login</a>
             </div>
+        </div>
+    )
+}
+
+function SideMenu() {
+    return (
+        <div className="dropdown-menu">
+            <ul className="side-links">
+                <a href="#"><li className="side-items"><i class="bi bi-collection-play"></i><p>Channels</p></li></a>
+                <a href="#"><li className="side-items"><i class="bi bi-translate"></i><p>Languages</p></li></a>
+                <a href="#"><li className="side-items"><i class="bi bi-collection-fill"></i><p>Geners</p></li></a>
+            </ul>
         </div>
     )
 }
